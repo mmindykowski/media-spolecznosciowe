378c5535a5b7a3361993266b454c5c0eb0cf6c38
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-18 20:28:03 +0200

    Zmiana plików na jsx oraz komponenty funkcyjne

---
+++ src/views/Login.jsx
@@ -0,0 +1,5 @@
+const Login = () => {
+    return <h2>Home</h2>;
+  };
+  
+  export default Login;
---
+++ src/views/SignUp.jsx
@@ -0,0 +1,5 @@
+const SignUp = () => {
+    return <h2>Home</h2>;
+  };
+  
+  export default SignUp;
