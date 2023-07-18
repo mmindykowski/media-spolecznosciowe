f956f265bc376a8097693baf354383728d7bcbaa
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-18 20:37:01 +0200

    import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Home from './views/Home';
+import Login from './views/Login';
+import SignUp from './views/SignUp';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
+      <Login/>
+      <SignUp/>
     </div>
   );
 }
