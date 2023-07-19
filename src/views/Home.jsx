319e4630efe14e93b1e1fab39476cf1705cae9b9
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-19 19:45:39 +0200

    instalacja axios plus obsluga

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getLatestPosts = () => {
+    axios
+      .post("http://akademia108.pl/api/social-app/post/latest")
+      .then((req) => {
+        setPosts(req.data);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getLatestPosts();
+  }, []);
+  console.log(posts);
   return <h2>Home</h2>;
 };
 
