49935810fa5f305943bbf3c2ba371f7b0aa1c651
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-19 20:09:38 +0200

    Zarys wyświetlania tablicy z postami

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,6 +4,13 @@ const Post = (props) => {
             <div className="avatar">
                 <img src={props.post.user.avatar_url} alt={props.post.user.username} />
             </div>
+            <div className="postData">
+                <div className="postMeta">
+                    <div className="author">{props.post.user.username}</div>
+                    <div className="date">{props.post.created_at.substring(0,10)}</div>
+                </div>
+                <div className="postContent"> <div className="author">{props.post.content}</div></div>
+            </div>
         </div>
     )
 }
