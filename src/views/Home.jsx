1547a333fa3582b37697177af110939714d15d3e
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-19 20:59:36 +0200

    ładowanie postów na stronie

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Post from "../components/Post";
 import { useEffect, useState } from "react";
-import './Home.css'
+import "./Home.css";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -9,8 +9,22 @@ const Home = () => {
   const getLatestPosts = () => {
     axios
       .post("http://akademia108.pl/api/social-app/post/latest")
-      .then((req) => {
-        setPosts(req.data);
+      .then((res) => {
+        setPosts(res.data);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const getNextPosts = () => {
+    axios
+      .post("http://akademia108.pl/api/social-app/post/older-then", {
+        date: posts[posts.length - 1].created_at,
+      })
+      .then((res) => {
+        setPosts(posts.concat(res.data));
       })
 
       .catch((error) => {
@@ -22,12 +36,14 @@ const Home = () => {
     getLatestPosts();
   }, []);
 
+  console.log(posts);
   return (
     <div className="home">
       <div className="postList">
         {posts.map((post) => {
-          return <Post post={post} />;
+          return <Post post={post} key={post.id} />;
         })}
+        <button onClick={getNextPosts}>Load more</button>
       </div>
     </div>
   );
