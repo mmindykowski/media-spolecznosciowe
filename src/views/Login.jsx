061c9de1c948a037969741f680bc9f190e690036
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-23 20:12:47 +0200

    Stworzenie formularza do logowania

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,13 @@
 const Login = () => {
-    return <h2>Login</h2>;
-  };
-  
-  export default Login;
+  return (
+    <div className="login">
+      <form>
+        <input type="text" name="username" placeholder="User name" />
+        <input type="password" name="password" placeholder="Password" />
+        <button className="btn">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
