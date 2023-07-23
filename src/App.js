d6afa510afced9a3e77f5cecc157ad8fa9412a5c
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-07-23 21:29:28 +0200

    Ukonczenie logowania

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import AppNav from "./components/AppNav";
 import AppRoutes from "./routes/AppRoutes";
+import Axios from "axios";
 
 function App() {
 
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+
+  Axios.defaults.headers.common['Authorization'] = "Bearer" + (user ? user.jwt_token : "");
 
   return (
     <div className="App">
