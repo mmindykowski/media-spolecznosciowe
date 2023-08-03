6ba3455cc4975f561c9663f7939bb6c07aa75142
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-03 17:53:05 +0200

    Wstawianie posta

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -5,18 +5,31 @@ import axios from "axios";
 const AddPost = (props) => {
   const [postContent, setPostContent] = useState("");
 
-
   // console.log(postContent);
-
 
   const addPost = (e) => {
     e.preventDefault();
-  };
 
+    if (!postContent) {
+      return;
+    }
+
+    axios
+      .post("https://akademia108.pl/api/social-app/post/add", {
+        content: postContent,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setPostContent('')
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="add-post">
-      <form>
+      <form onSubmit={addPost}>
         <textarea
           placeholder="Dodaj post..."
           onChange={(e) => setPostContent(e.target.value)}
