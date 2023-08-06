7e79e045f0008271e60d316a97c869c33c7f4d0a
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-06 11:38:16 +0200

    przekazanie usera postów

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -57,7 +57,7 @@ const Home = (props) => {
 
       <div className="postList">
         {posts.map((post) => {
-          return <Post post={post} key={post.id} />;
+          return <Post post={post} key={post.id} user={props.user} />;
         })}
         <button className="btn loadMore" onClick={getNextPosts}>
           Load more
