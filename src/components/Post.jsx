6e0d321f36d406afc595c38b246419161adfdc15
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-06 11:45:51 +0200

    przycisk usuń tylko przy autorze posta

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,27 +1,34 @@
 import { useState } from "react";
-import './Post.css';
+import "./Post.css";
 
 const Post = (props) => {
+  const [likesCount, setLikesCount] = useState(props.post.likes.length);
 
-    const [likesCount, setLikesCount] = useState(props.post.likes.length)
-
-    return (
-        <div className="post">
-            <div className="avatar">
-                <img src={props.post.user.avatar_url} alt={props.post.user.username} />
-            </div>
-            <div className="postData">
-                <div className="postMeta">
-                    <div className="author">{props.post.user.username}</div>
-                    <div className="date">{props.post.created_at.substring(0,10)}</div>
-                </div>
-                <div className="postContent"> <div className="author">{props.post.content}</div></div>
-                <div className="likes">
-                    {likesCount}
-                </div>
-            </div>
+  return (
+    <div className="post">
+      <div className="avatar">
+        <img src={props.post.user.avatar_url} alt={props.post.user.username} />
+      </div>
+      <div className="postData">
+        <div className="postMeta">
+          <div className="author">{props.post.user.username}</div>
+          <div className="date">{props.post.created_at.substring(0, 10)}</div>
         </div>
-    )
-}
+        <div className="postContent">
+          {" "}
+          <div className="author">{props.post.content}</div>
+        </div>
+        <div className="likes">
+          {/* Uzytkownik przestał istniec (null albo undefined) wiec uzywamy operatora znaku zapytania, zeby nie wywalalo bledu jak bedzie uzytkownik wylogowany   */}
+
+          {props.user?.username === props.post.user.username && (
+            <button className="btn">Usuń</button>
+          )}
+          {likesCount}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Post;
