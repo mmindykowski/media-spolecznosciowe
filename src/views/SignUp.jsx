c6a5d2890c7d5ba166dbbdb0eb1d21a4732d2f33
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-16 20:09:34 +0200

    pobieranie danych z inputów

--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,17 +1,57 @@
-import './Signup.css'
-import {Navigate} from "react-router-dom"
+import { useState } from "react";
+import "./Signup.css";
+import { Navigate } from "react-router-dom";
 
 const Signup = (props) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleInputChange = (e) => {
+    const target = e.target;
+    const name = target.name;
+
+    setFormData({
+      // Spread syntax - kopiuje caly obiekt
+      ...formData,
+      [name]: target.value,
+    });
+  };
+
+  // console.log(formData);
   return (
     <div className="signup">
-      {props.user && <Navigate to="/"/>}
-    <form>
-      <input type="text" name="username" placeholder="User name"/>
-      <input type="email" name="email" placeholder="E-mail"/>
-      <input type="password" name="password" placeholder="Password"/>
-      <input type="password" name="confirmPassword" placeholder="Confirm password"/>
-      <button className='btn'>Sign up</button>
-    </form>
+      {props.user && <Navigate to="/" />}
+      <form>
+        <input
+          type="text"
+          name="username"
+          placeholder="User name"
+          onChange={handleInputChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm password"
+          onChange={handleInputChange}
+        />
+        <button className="btn">Sign up</button>
+      </form>
     </div>
   );
 };
