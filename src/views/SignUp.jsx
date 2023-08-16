71a581fff229cae4941c1a5ae442885625fe2496
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-16 19:30:24 +0200

    Poprawiony form

--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,7 +1,7 @@
 const Signup = () => {
   return (
     <div className="signup">
-    <form action="">
+    <form>
       <input type="text" name="username" placeholder="User name"/>
       <input type="email" name="email" placeholder="E-mail"/>
       <input type="password" name="password" placeholder="Password"/>
